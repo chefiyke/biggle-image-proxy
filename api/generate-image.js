3a2bb64162d8b44b5f08751fbcce556ec5f2f50e
chefiyke <79388174+chefiyke@users.noreply.github.com>
2026-04-25 19:11:21 +0100

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -22,6 +22,9 @@ export default async function handler(req, res) {
     })
   });
 
-  const data = await response.json();
-  res.status(200).json(data);
+  const text = await response.text();
+
+return res.status(200).json({
+  raw: text
+});
 }
