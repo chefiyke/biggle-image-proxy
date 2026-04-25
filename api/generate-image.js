31e1fc2017fd374316df83862d43d83918a5ce40
chefiyke <79388174+chefiyke@users.noreply.github.com>
2026-04-25 19:16:07 +0100

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     body: JSON.stringify({
       model: "gpt-image-1",
       prompt,
-      size: "512x512"
+      size: "1024x1024"
     })
   });
 
